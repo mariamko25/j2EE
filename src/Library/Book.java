354329561e9e92167b4c4b4ca88d1f888677229f
Konaté Mariam <32955466+mariamko25@users.noreply.github.com>
2018-03-10 23:29:26 +0100

    Update Book.java

--- src/Library/Book.java
+++ src/Library/Book.java
@@ -1,12 +1,24 @@
 package Library;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
+/**
+ * 
+ * @author MariamKonate
+ * Class to represent a Book
+ */
 @Entity(name="Book")
-public class Book {
+public class Book implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "BookID", updatable = false, nullable = false)
 	int ID;
 	
@@ -30,4 +42,153 @@ public class Book {
 	
 	@Column(name="BookGenres")
 	BookGenres BookGenres;
+
+	/**
+	 * Constructor with no arguments
+	 */
+	Book()
+	{
+		
+	}
+	
+	/**
+	 * Constructor to copy
+	 * @param B
+	 */
+	Book(Book B)
+	{
+		Author=B.getAuthor();
+		Title=B.getTitle();
+		Private=B.isPrivate();
+		Borrowed=B.isBorrowed();
+		Year=B.getYear();
+		PageNumber=B.getPageNumber();
+		BookGenres=B.getBookGenres();
+	}
+	
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	/**
+	 * 
+	 * @return ID
+	 */
+	public int getID() {
+		return ID;
+	}
+
+	/**
+	 * 
+	 * @return Author
+	 */
+	public Author getAuthor() {
+		return Author;
+	}
+
+	/**
+	 * 
+	 * @param author
+	 */
+	public void setAuthor(Author author) {
+		Author = author;
+	}
+
+	/**
+	 * 
+	 * @return Title
+	 */
+	public String getTitle() {
+		return Title;
+	}
+
+	/**
+	 * 
+	 * @param title
+	 */
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	/**
+	 * 
+	 * @return Private
+	 */
+	public boolean isPrivate() {
+		return Private;
+	}
+
+	/**
+	 * 
+	 * @param private1
+	 */
+	public void setPrivate(boolean private1) {
+		Private = private1;
+	}
+
+	/**
+	 * 
+	 * @return Borrowed
+	 */
+	public boolean isBorrowed() {
+		return Borrowed;
+	}
+
+	/**
+	 * 
+	 * @param borrowed
+	 */
+	public void setBorrowed(boolean borrowed) {
+		Borrowed = borrowed;
+	}
+
+	/**
+	 * 
+	 * @return Year
+	 */
+	public int getYear() {
+		return Year;
+	}
+
+	/**
+	 * 
+	 * @param year
+	 */
+	public void setYear(int year) {
+		Year = year;
+	}
+
+	/**
+	 * 
+	 * @return PageNumber
+	 */
+	public int getPageNumber() {
+		return PageNumber;
+	}
+
+	/**
+	 * 
+	 * @param pageNumber
+	 */
+	public void setPageNumber(int pageNumber) {
+		PageNumber = pageNumber;
+	}
+
+	/**
+	 * 
+	 * @return BookGenres
+	 */
+	public BookGenres getBookGenres() {
+		return BookGenres;
+	}
+
+	/**
+	 * 
+	 * @param bookGenres
+	 */
+	public void setBookGenres(BookGenres bookGenres) {
+		BookGenres = bookGenres;
+	}
+
+	
 }
