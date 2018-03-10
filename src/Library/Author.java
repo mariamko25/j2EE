068b557392f7dc5e1e72c20fc771e47beb0f588f
Konaté Mariam <32955466+mariamko25@users.noreply.github.com>
2018-03-10 23:28:50 +0100

    Update Author.java

--- src/Library/Author.java
+++ src/Library/Author.java
@@ -1,12 +1,57 @@
 package Library;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
+/**
+ * 
+ * @author MariamKonate
+ * Class representing an author
+ */
 @Entity(name="Author")
-public class Author extends Person{
+public class Author extends Person implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 	@Column(name="nbBooks")
 	int nbBooks;
+	
+	/**
+	 * Constructor with no arguments
+	 */
+	Author()
+	{
+		super();
+	}
+	
+	/**
+	 * Constructor to copy
+	 * @param A
+	 */
+	Author(Author A)
+	{
+		super(A);
+		nbBooks=A.getNbBooks();
+	}
 
+	/**
+	 * 
+	 * @return nBooks
+	 */
+	public int getNbBooks() {
+		return nbBooks;
+	}
 
+	/**
+	 * 
+	 * @param nbBooks
+	 */
+	public void setNbBooks(int nbBooks) {
+		this.nbBooks = nbBooks;
+	}
+	
 }
