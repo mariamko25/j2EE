0a34e16cc851540b2ba21e8e25c495bf305f0f28
Konaté Mariam <32955466+mariamko25@users.noreply.github.com>
2018-03-10 23:37:49 +0100

    Update Catalog.java

--- src/Library/Catalog.java
+++ src/Library/Catalog.java
@@ -1,14 +1,26 @@
 package Library;
 
+import java.io.Serializable;
 import java.util.*;
 
 import javax.persistence.*;
 
+/**
+ * 
+ * @author MariamKonate
+ * Class representing a Catalog
+ */
 @Entity(name="Catalog")
-public class Catalog {
+public class Catalog implements Serializable
+{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "CatalogID", updatable = false, nullable = false)
 	int CatalogID;
 	
@@ -17,8 +29,63 @@ public class Catalog {
 	
 	@Column(name="Description")
 	String description;
-	
-	
-	
 
+	/**
+	 * Constructor with no arguments
+	 */
+	Catalog()
+	{
+		
+	}
+	
+	/**
+	 * Constructor to copy
+	 * @param C
+	 */
+	Catalog(Catalog C)
+	{
+		Books=C.getBooks();
+		description= C.getDescription();
+	}
+	
+	/**
+	 * 
+	 * @return CatalogID
+	 */
+	public int getCatalogID() {
+		return CatalogID;
+	}
+
+	/**
+	 * 
+	 * @return Books
+	 */
+	public Set<Book> getBooks() {
+		return Books;
+	}
+
+	/**
+	 * 
+	 * @param books
+	 */
+	public void setBooks(Set<Book> books) {
+		Books = books;
+	}
+
+	/**
+	 * 
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * 
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
