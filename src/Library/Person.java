a0f74d4ea11be48e69344c97287cf87a199e1ffb
Konaté Mariam <32955466+mariamko25@users.noreply.github.com>
2018-03-10 23:40:12 +0100

    Update Person.java

--- src/Library/Person.java
+++ src/Library/Person.java
@@ -1,14 +1,25 @@
 package Library;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
-
+/**
+ * 
+ * @author MariamKonate
+ * class implementing a person
+ */
 @MappedSuperclass
-public class Person 
+public class Person implements Serializable
 {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "PersonID", updatable = false, nullable = false)
 	int ID;
 	
@@ -24,10 +35,100 @@ public class Person
 	@Column(name="Age")
 	String Age;
 	
+
+	/**
+	 * constructor with no arguments
+	 */
 	Person()
 	{
 		
 	}
 	
+	/**
+	 * constructor to copy
+	 * @param p
+	 */
+	Person(Person p)
+	{
+		Name=p.getName();
+		Phone_Number=p.getPhone_Number();
+		Email_Address=p.getEmail_Address();
+		Age=p.getAge();
+		
+	}
 	
+	/**
+	 * 
+	 * @return ID
+	 */
+	public int getID() {
+		return ID;
+	}
+
+	
+
+	/**
+	 * 
+	 * @return Name
+	 */
+	public String getName() {
+		return Name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		Name = name;
+	}
+
+	/**
+	 * 
+	 * @return Phone_Number
+	 */
+	public String getPhone_Number() {
+		return Phone_Number;
+	}
+
+	/**
+	 * 
+	 * @param phone_Number
+	 */
+	public void setPhone_Number(String phone_Number) {
+		Phone_Number = phone_Number;
+	}
+
+	/**
+	 * 
+	 * @return Email_Address
+	 */
+	public String getEmail_Address() {
+		return Email_Address;
+	}
+
+	/**
+	 * 
+	 * @param email_Address
+	 */
+	public void setEmail_Address(String email_Address) {
+		Email_Address = email_Address;
+	}
+
+	/**
+	 * 
+	 * @return Age
+	 */
+	public String getAge() {
+		return Age;
+	}
+
+	/**
+	 * 
+	 * @param age
+	 */
+	public void setAge(String age) {
+		Age = age;
+	}
+
 }
